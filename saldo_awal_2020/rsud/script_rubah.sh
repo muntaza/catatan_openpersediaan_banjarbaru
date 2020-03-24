f4768d0e0f0f4aed10c9a9154f4e753a13128f5f
Muhammad Muntaza <muhammad@muntaza.id>
2020-03-24 09:51:49 +0800

    data rsud

--- saldo_awal_2020/rsud/script_rubah.sh
+++ saldo_awal_2020/rsud/script_rubah.sh
@@ -1,1 +1,2 @@
-cat obat_bmhp_rsud.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87044\,/g -e s/$/\)\;/g > obat_bmhp_rsud_insert.sql
+cat rsud_skpd.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87044\,/g -e s/$/\)\;/g > rsud_skpd_insert.sql
+cat rsud_blud.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87044\,/g -e s/$/\)\;/g > rsud_blud_insert.sql
