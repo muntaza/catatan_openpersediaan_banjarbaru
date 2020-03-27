6b886c0f4cc1ca28e7f52902bdcf5c78db125a1a
Muhammad Muntaza <muhammad@muntaza.id>
2020-03-27 09:54:26 +0800

    pkm banjarbaru utara

--- saldo_awal_2020/puskesmas/script_rubah.sh
+++ saldo_awal_2020/puskesmas/script_rubah.sh
@@ -1,2 +1,1 @@
-cat rsud_skpd.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(65712\,/g -e s/$/\)\;/g > rsud_skpd_insert.sql
-cat rsud_blud.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(65713\,/g -e s/$/\)\;/g > rsud_blud_insert.sql
+cat pkm_banjarbaru_utara.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(65712\,/g -e s/$/\)\;/g > pkm_banjarbaru_utara_insert.sql
