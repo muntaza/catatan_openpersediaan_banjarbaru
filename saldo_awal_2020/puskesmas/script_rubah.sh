3ef8aee9cbe5e011b1c6806422e14574fc8321c7
Muhammad Muntaza <muhammad@muntaza.id>
2020-03-27 10:22:21 +0800

    PKM Rawat Inap Cempaka

--- saldo_awal_2020/puskesmas/script_rubah.sh
+++ saldo_awal_2020/puskesmas/script_rubah.sh
@@ -1,2 +1,3 @@
 cat pkm_banjarbaru_utara.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(65879\,/g -e s/$/\)\;/g > pkm_banjarbaru_utara_insert.sql
 cat pkm_banjarbaru_selatan.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(65881\,/g -e s/$/\)\;/g > pkm_banjarbaru_selatan_insert.sql
+cat pkm_rawat_inap_cempaka.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(65885\,/g -e s/$/\)\;/g > pkm_rawat_inap_cempaka_insert.sql
