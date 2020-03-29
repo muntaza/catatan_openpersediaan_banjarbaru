8728d840af61ceb44a27804be2aafd8561e08838
Muhammad Muntaza <muhammad@muntaza.id>
2020-03-29 10:46:15 +0800

    dinas pupr

--- saldo_awal_2020/skpd/script_rubah.sh
+++ saldo_awal_2020/skpd/script_rubah.sh
@@ -1,4 +1,4 @@
 cat dinas_kesehatan.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(66075\,/g -e s/$/\)\;/g > dinas_kesehatan_insert.sql
 cat dinas_perkim.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(66076\,/g -e s/$/\)\;/g > dinas_perkim_insert.sql
 cat dinas_dukcatpil.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(66077\,/g -e s/$/\)\;/g > dinas_dukcatpil_insert.sql
-#cat dinas_pupr.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(66078\,/g -e s/$/\)\;/g > dinas_pupr_insert.sql
+cat dinas_pupr.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(66078\,/g -e s/$/\)\;/g > dinas_pupr_insert.sql
