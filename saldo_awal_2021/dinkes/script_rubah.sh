78271a94bb212135df14979000a0ee07a3872fe5
Muhammad Muntaza <muhammad@muntaza.id>
2021-03-15 06:29:58 +0800

    add

--- saldo_awal_2021/dinkes/script_rubah.sh
+++ saldo_awal_2021/dinkes/script_rubah.sh
@@ -1,4 +1,4 @@
-cat rsud_skpd_btt.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(1\,/g -e s/$/\)\;/g > rsud_skpd_btt_insert.sql
-cat rsud_skpd_hibah_pihak_ketiga.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(2\,/g -e s/$/\)\;/g > rsud_skpd_hibah_pihak_ketiga_insert.sql
-cat rsud_skpd_umum.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(3\,/g -e s/$/\)\;/g > rsud_skpd_umum_insert.sql
-cat rsud_blud_umum.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(4\,/g -e s/$/\)\;/g > rsud_blud_umum_insert.sql
+cat dinkes_umum.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(9\,/g -e s/$/\)\;/g > dinkes_umum_insert.sql
+cat dinkes_btt.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(10\,/g -e s/$/\)\;/g > dinkes_btt_insert.sql
+cat dinkes_hibah_pihak_ketiga.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11\,/g -e s/$/\)\;/g > dinkes_hibah_pihak_ketiga_insert.sql
+cat dinkes_hibah_provinsi.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(12\,/g -e s/$/\)\;/g > dinkes_hibah_provinsi_insert.sql
